c47b9269db9ea34eba01e6e548090b0f00e72f8c
Robin Müller <robin.mueller@outlook.de>
2021-11-12 12:44:27 +0100

    Added Error Handling

--- Azure Active Directory/Accounts/New-SurfaceHubAccount.ps1
+++ Azure Active Directory/Accounts/New-SurfaceHubAccount.ps1
@@ -11,7 +11,9 @@ param (
     [string]$DisplayName,
 
     [ValidateLength(2,2)]
-    [string]$UsageLocation = 'DE'
+    [string]$UsageLocation = 'DE',
+
+    [switch]$Force
 )
 
 $Alias = ($UPN -split '@')[0]
@@ -23,19 +25,54 @@ $Alias = ($UPN -split '@')[0]
 'Connecting...' | Write-Verbose
 
 Connect-ExchangeOnline -ShowBanner:$false
-Connect-AzureAD
+Connect-AzureAD | Out-Null
 
 #endregion connect
 
-'Creating New Mailbox {0}' -f $UPN | Write-Verbose
+'Checking if Mailbox alreay exists' | Write-Verbose
 
-New-Mailbox -MicrosoftOnlineServicesID $UPN `
-            -Alias $Alias `
-            -Name $DisplayName `
-            -Room `
-            -EnableRoomMailboxAccount $true `
-            -RoomMailboxPassword $RoomPassword `
-    | Out-Null
+$Mailbox = Get-Mailbox -Identity $Alias -ErrorAction SilentlyContinue
+
+if ($null -ne $Mailbox) {
+    'Mailbox found, checking parameters' | Write-Verbose
+    
+    if ($Mailbox.MicrosoftOnlineServicesID -ne $UPN) {
+        'Mailbox has a different UPN: {0}' -f $Mailbox.MicrosoftOnlineServicesID | Write-Warning
+    }
+    
+    if ($Mailbox.DisplayName -ne $DisplayName) {
+        'Mailbox has a different DisplayName: {0}' -f $Mailbox.DisplayName | Write-Warning
+    }
+
+    if (-not $Mailbox.IsMailboxEnabled -or -not $Mailbox.RoomMailboxAccountEnabled) {
+        'Mailbox is disabled' | Write-Warning
+    }
+
+    'Password cannot be checked' | Write-Warning
+
+    if ($Force) {
+        $Mailbox | Set-Mailbox -MicrosoftOnlineServicesID $UPN `
+                               -DisplayName $DisplayName `
+                               -EnableRoomMailboxAccount $true `
+                               -RoomMailboxPassword $RoomPassword `
+                               -AccountDisabled $false `
+                               -Force `
+                               -ErrorAction Stop
+    } else {
+        throw 'Mailbox already exists, if you want to override the propertirs use -Force'
+    }
+} else {
+    'Creating New Mailbox {0}' -f $UPN | Write-Verbose
+
+    New-Mailbox -MicrosoftOnlineServicesID $UPN `
+                -Alias $Alias `
+                -DisplayName $DisplayName `
+                -Room `
+                -EnableRoomMailboxAccount $true `
+                -RoomMailboxPassword $RoomPassword `
+                -ErrorAction Stop `
+        | Out-Null
+}
 
 'Setting Calendar properties' | Write-Verbose
 
@@ -47,7 +84,8 @@ Set-CalendarProcessing -Identity $UPN `
                        -DeleteSubject $false `
                        -RemovePrivateProperty $false `
                        -AddAdditionalResponse $true `
-                       -AdditionalResponse "This room is equipped with a Surface Hub"
+                       -AdditionalResponse 'This room is equipped with a Surface Hub' `
+                       -ErrorAction Stop
 
 $i = 0
 do {
@@ -59,14 +97,15 @@ do {
     
 
 if ($null -eq $user) {
-    throw 'New User not found 🤦'
+    throw 'New User not found after creation. Meybe we need to give the cloud more time to process and repeat in a bit.'
 }
 
 'Setting PasswordExpiration and Usage Location' | Write-Verbose
 
 Set-AzureADUser -ObjectId $UPN `
                 -PasswordPolicies "DisablePasswordExpiration" `
-                -UsageLocation $UsageLocation
+                -UsageLocation $UsageLocation  `
+                -ErrorAction Stop
 
 'Adding License' | Write-Verbose
 
@@ -76,4 +115,4 @@ $License.SkuId = "6070a4c8-34c6-4937-8dfb-39bbc6397a60"
 $AssignedLicenses = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
 $AssignedLicenses.AddLicenses = $License
 
-Set-AzureADUserLicense -ObjectId $UPN -AssignedLicenses $AssignedLicenses
+Set-AzureADUserLicense -ObjectId $UPN -AssignedLicenses $AssignedLicenses -ErrorAction Stop
